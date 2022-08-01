eb0c8524aa2981378899999917b9a289153d67b0
Davi Aleixo <davialeixo_nogueira@outlook.com>
2022-08-01 19:14:58 -0300

    CheckMate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,16 +56,19 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
+		if(!chessMatch.getCheckMate()) {
 		System.out.println("Waiting player : " + chessMatch.getCurrentPlayer());
 		
 		if( chessMatch.getCheck()) {
 			System.out.println("CHECK!");
 		}
-
-		
 	}
-	
-	
+	else {
+		System.out.println("CHECKMATE!");
+		System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+	}
+}
+		
 
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int col = 0; col < pieces.length; col++) {
